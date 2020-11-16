2c36b0d8ac0da3bd8306de5b26c8dca5b48b6292
Sarah250 <Sarah250@github.com>
2020-11-16 22:07:43 +0000

    #4 #1
    Created User's Data Model and User's Registration Endpoint

--- Backend/User/config/server.js
+++ Backend/User/config/server.js
@@ -20,7 +20,7 @@ const mongoose      = require('mongoose');
 mongoose.connect(process.env.MONGO_CONNECTION, {
     useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
     .then(() => {
-        console.log("Connection to MongoDB successfully established")
+        console.log("Connection to MongoDB successfully established");
     })
     .catch(() => {
         throw new Error("Could not establish connection to MongoDB");
