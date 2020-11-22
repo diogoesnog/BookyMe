3cb0989cd026084d850e2eb6d6bb2a1301092559
Miguel Solans <dev.miguelsolans@gmail.com>
2020-11-22 21:20:53 +0000

    Created a Request Wrapper and Fixed Register Process

--- Backend/Gateway/app/routes/api/user.js
+++ Backend/Gateway/app/routes/api/user.js
@@ -2,24 +2,19 @@ const express = require('express');
 const router = express.Router();
 const http = require('wretch');
 const User = require('../../services/users');
-
-
 const { validator } = require('../../middlewares/checkBody');
 
 router.post('/register', validator([
     "name", "username", "email", "password"
 ]), (req, res) => {
     let body = JSON.stringify(req.body);
-    http(`${process.env.USER_SERVICE_ENDPOINT}/user/register`)
-        .post(body)
-        .res(response => {
-            console.log("=== Success ===");
-            console.log(response);
-        }).catch(err => {
-            console.log("=== Error === ")
-            console.log(err);
 
-    })
+    User.register(body)
+        .then(response => {
+            res.status(response.status).jsonp(response.data);
+        }).catch(err => {
+            res.status(err.status).jsonp(err.data);
+        });
 });
 
 router.post('/login', validator([
@@ -32,8 +27,8 @@ router.post('/login', validator([
             res.setHeader('Authorization', data.cookies[0]);
             res.status(data.response.status).jsonp(data.response);
         }).catch(err => {
-
-            res.status(err.status).jsonp(err);
+            let status = err.status || 500;
+            res.status(status).jsonp(err);
         });
 });
 
---
+++ Backend/Gateway/app/utils/Request.js
@@ -0,0 +1,65 @@
+// "use strict";
+// const fetch = require('node-fetch');
+const fetch = require('node-fetch');
+
+class Request {
+    constructor(url) {
+        // this.fetch = require('node-fetch');
+        this.url = url;
+        this.method = "";
+        this.options = {};
+        this.body = null;
+        this.headers = {};
+    }
+
+    get() {
+        return this._request('GET');
+    }
+
+    post(body) {
+        this.body = body;
+        return this._request('POST');
+    }
+
+    put(body) {
+        this.body = body;
+        return this._request("PUT");
+    }
+
+    setHeaders(headers) {
+        this.headers = headers;
+    }
+
+    _request(method) {
+        return new Promise((resolve, reject) => {
+            let bodyStr = JSON.stringify(this.body);
+            console.log(bodyStr);
+            fetch(this.url, {
+                method: method,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    ...this.headers
+                },
+                body: this.body
+            }).then(response => {
+                return response.json()
+                    .then(json => {
+                        let resObj = {
+                            status: response.status,
+                            headers: response.headers,
+                            data: json
+                        };
+
+                        if(response.ok) {
+                            resolve(resObj);
+                        } else {
+                            reject(resObj);
+                        }
+                    });
+            });
+        });
+    }
+}
+
+module.exports = Request;
