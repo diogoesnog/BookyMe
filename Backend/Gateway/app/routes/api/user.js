68097d2c116ff0662e7f9eb3474601a2747ded1f
miguelsolans <dev.miguelsolans@gmail.com>
2020-11-28 18:37:32 +0000

    Gateway Documentation

--- Backend/Gateway/app/routes/api/user.js
+++ Backend/Gateway/app/routes/api/user.js
@@ -1,9 +1,49 @@
 const express = require('express');
 const router = express.Router();
-const http = require('wretch');
 const User = require('../../services/users');
 const { validator } = require('../../middlewares/checkBody');
+const swaggerDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
+
+/**
+ * @swagger
+ * /users/register:
+ *   post:
+ *     description: Use to create a new account
+ *     consumes:
+ *        - "application/json"
+ *     produces:
+ *        - "application/json"
+ *     parameters:
+ *        - in: body
+ *          name: user
+ *          description: User information's
+ *          schema:
+ *              type: Object
+ *              required:
+ *                  - name
+ *                  - username
+ *                  - email
+ *                  - password
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                  username:
+ *                      type: string
+ *                  email:
+ *                      type: string
+ *                  password:
+ *                      type: string
+ *     responses:
+ *        '201':
+ *           description: Account created successfully
+ *           content:
+ *              application/json
+ *        '5XX':
+ *            description: Server Error
+ *
+ */
 router.post('/register', validator([
     "name", "username", "email", "password"
 ]), (req, res) => {
@@ -17,6 +57,42 @@ router.post('/register', validator([
         });
 });
 
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     description: Endpoint for User Authentication
+ *     consumes:
+ *        - "application/json"
+ *     produces:
+ *        - "application/json"
+ *     parameters:
+ *        - in: body
+ *          name: user
+ *          description: User information's
+ *          schema:
+ *              type: Object
+ *              required:
+ *                  - email
+ *                  - password
+ *              properties:
+ *                  email:
+ *                      type: string
+ *                  password:
+ *                      type: string
+ *     responses:
+ *        '201':
+ *           description: Successfully authenticated. A User token has been passed over headers
+ *           content:
+ *              application/json
+ *           headers:
+ *              Authentication:
+ *                 schema:
+ *                    type: string
+ *        '5XX':
+ *            description: Server Error
+ *
+ */
 router.post('/login', validator([
     'email', 'password'
 ]), (req, res) => {
