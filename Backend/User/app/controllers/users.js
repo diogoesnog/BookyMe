a6de571b67df5a16ad0b9eb24745653db33e889b
Tifany Silva <36680463+Sarah250@users.noreply.github.com>
2020-12-02 15:41:25 +0000

    Updated Register Endpoint (#31)
    * Update User's Information (closes #12 )
    
    Now updating user's password, email, name and address (missing username).
    
    * Updated Register Endpoint
    
    User data model updated : zipCode and city fields added;
    Register endpoint is now capable of adding city and zipCode.
    Updated Register in FrontEnd in order to integrate city and zipCode
    
    * Update index.js
    
    Co-authored-by: Sarah250 <Sarah250@github.com>

--- Backend/User/app/controllers/users.js
+++ Backend/User/app/controllers/users.js
@@ -1,10 +1,10 @@
 const User = require('../models/user');
 
 
-// TODO: atualizar infos, mudar de pass, addFavorites
-module.exports.createUser = ({name, username, email, address, password, type}) => {
-    const user = new User({name : name, username: username, address : address, email: email, password : password, type: type});
-    
+// TODO: addFavorites, addPicture, password
+module.exports.createUser = ({name, username, email, address, city,zipCode, password, type}) => {
+    const user = new User({name : name, username: username, address : address, city: city, zipCode: zipCode, email: email, password : password, type: type});
+
     return user.save();
 }
 
@@ -19,12 +19,15 @@ module.exports.findById = (id) => {
 
 module.exports.updateEmail = (id, email) =>  {
     return User.findByIdAndUpdate(id, {email: email}, {new: true});
-}
+
 
 module.exports.updatePassword = (id, password) => {
     return User.findByIdAndUpdate(id, {password: password}, {new: true});
 }
 
+module.exports.deleteAll = () => {
+    return User.deleteMany();
+}
 
 module.exports.getUsers = ( ()=> {
     return User.find();
