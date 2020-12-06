52b9f030c1e75b36225ee81dbc263fd3c7842b01
miguelsolans <dev.miguelsolans@gmail.com>
2020-12-06 00:26:31 +0000

    Request with better Error Handler

--- Backend/Gateway/app/utils/Request.js
+++ Backend/Gateway/app/utils/Request.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const fetch = require('node-fetch');
+const FormData = require('form-data');
+const fs = require('fs');
+const path = require('path');
 
 class Request {
     /**
@@ -14,6 +17,35 @@ class Request {
         this.body =     null;
         this.headers =  {};
         this.params =   {};
+        this.contentType = null;
+    }
+
+    // Content Types
+    isJson() {
+        this.contentType = "application/json";
+    }
+
+    isPlainText() {
+        this.contentType = "text/plain";
+    }
+
+    isUrlencoded() {
+        this.contentType = "application/x-www-form-urlencoded";
+    }
+
+    isMultipart() {
+        this.contentType = "multipart/form-data";
+    }
+
+    // TODO: come out with a better solution for file uploads
+    uploadMedia(field, file) {
+        this.isMultipart();
+        let form = new FormData();
+        let filePath = path.resolve(file.path);
+        form.append(field, fs.createReadStream(filePath));
+        this.body = form;
+
+        return this._request("POST");
     }
 
     /**
@@ -123,8 +155,8 @@ class Request {
             fetch(url, {
                 method: method,
                 headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
+                    // 'Content-Type': this.contentType,
+                    //'Accept': 'application/json',
                     ...this.headers
                 },
                 body: this.body
@@ -143,6 +175,11 @@ class Request {
                             reject(resObj);
                         }
                     });
+            }).catch(err => {
+                reject({
+                    status: 500,
+                    data: err
+                });
             });
         });
     }
