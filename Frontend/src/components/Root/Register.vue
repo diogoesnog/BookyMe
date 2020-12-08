2a648b478672a21adadb474d2cecee11bb38f078
minesvieira <56404783+minesvieira@users.noreply.github.com>
2020-12-08 20:20:38 +0000

    inputs do form atualizados

--- Frontend/src/components/Root/Register.vue
+++ Frontend/src/components/Root/Register.vue
@@ -3,46 +3,77 @@
     <h4  class="text-center" style="font-weight: bold;">{{ $t('registerPage.title') }}</h4>
     <p class="text-center" v-html="$t('registerPage.description')"></p>
     <q-form>
-      <q-input rounded outlined :label="$t('registerPage.fullName')" v-model="user.name" type="text" style="width: 35%;">
-        <template v-slot:prepend>
-          <q-icon name="fas fa-user" />
-        </template>
-      </q-input>
-      <q-input  rounded outlined :label="$t('registerPage.username')" v-model="user.username" type="text" style="width: 35%;">
-        <template v-slot:prepend>
-          <q-icon name="fas fa-hashtag" />
-        </template>
-      </q-input>
-      <q-input  rounded outlined :label="$t('registerPage.email')" v-model="user.email" type="text">
-        <template v-slot:prepend>
-          <q-icon name="fas fa-at" />
-        </template>
-      </q-input>
-      <q-input  rounded outlined :label="$t('registerPage.address')" v-model="user.address" type="text">
-        <template v-slot:prepend>
-          <q-icon name="fas fa-home" />
-        </template>
-      </q-input>
-      <q-input  rounded outlined :label="$t('registerPage.city')" v-model="user.city" type="text">
-        <template v-slot:prepend>
-          <q-icon name="fas fa-home" />
-        </template>
-      </q-input>
-      <q-input  rounded outlined :label="$t('registerPage.postalCode')" v-model="user.zipCode" type="text">
-        <template v-slot:prepend>
-          <q-icon name="fas fa-home" />
-        </template>
-      </q-input>
-      <q-input  rounded outlined :label="$t('registerPage.password')" v-model="user.password" type="password">
-        <template v-slot:prepend>
-          <q-icon name="fas fa-lock" />
-        </template>
-      </q-input>
-      <br/>
-      <q-btn class="absolute-center" rounded standout push color="light-blue-7" @click="handleRegister" style="width: 35%; margin-top:53%" >
-        {{ $t('registerPage.registerAccount') }}
-      </q-btn>
+      <div class="row">
+        <div class="col-6">
+          <q-input rounded outlined :label="$t('registerPage.fullName')" v-model="user.name" type="text" style="width: 90%;">
+            <template v-slot:prepend>
+              <q-icon name="fas fa-user" />
+            </template>
+          </q-input>
+        </div>
+        <br>
+        <div class="col-6">
+          <q-input  rounded outlined :label="$t('registerPage.username')" v-model="user.username" type="text" style="width: 90%;">
+            <template v-slot:prepend>
+              <q-icon name="fas fa-hashtag" />
+            </template>
+          </q-input>
+          <br>
+        </div>
+        <div class="col-12">
+          <q-input  rounded outlined :label="$t('registerPage.email')" v-model="user.email" type="text" style="width: 95%;">
+            <template v-slot:prepend>
+              <q-icon name="fas fa-at" />
+            </template>
+          </q-input>
+          <br>
+        </div>
+          <div class="col-12">
+            <q-input  rounded outlined :label="$t('registerPage.address')" v-model="user.address" type="text" style="width: 95%;">
+              <template v-slot:prepend>
+                <q-icon name="fas fa-home" />
+              </template>
+              <br>
+            </q-input>
+            <br>
+          </div>
+        <div class="col-6">
+          <q-input  rounded outlined :label="$t('registerPage.city')" v-model="user.city" type="text" style="width: 90%;">
+            <template v-slot:prepend>
+              <q-icon name="fas fa-home" />
+            </template>
+          </q-input>
+          <br>
+        </div>
+        <div class="col-6">
+          <q-input  rounded outlined :label="$t('registerPage.postalCode')" v-model="user.zipCode" type="text" style="width: 90%;">
+            <template v-slot:prepend>
+              <q-icon name="fas fa-home" />
+            </template>
+          </q-input>
+        </div>
+        <div class="col-6">
+          <q-input  rounded outlined :label="$t('registerPage.password')" v-model="user.password" type="password" style="width: 90%;">
+            <template v-slot:prepend>
+              <q-icon name="fas fa-lock" />
+            </template>
+          </q-input>
+        </div>
+        <div class="col-6">
+          <q-input  rounded outlined :label="$t('registerPage.confirmPassword')" v-model="user.password" type="confirmpassword" style="width: 90%;">
+            <template v-slot:prepend>
+              <q-icon name="fas fa-lock" />
+            </template>
+          </q-input>
+          <br>
+        </div>
+          <br/>
+          <q-btn class="absolute-center" rounded standout push color="light-blue-7" @click="handleRegister" style="width: 35%; margin-top:53%" >
+            {{ $t('registerPage.registerAccount') }}
+          </q-btn>
+      </div>
     </q-form>
+    
   </div>
 </template>
 
