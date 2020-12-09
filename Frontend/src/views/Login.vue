94f209952b0bff8dabc5e2cf6291cfec560678b4
Diogo Nogueira <34174814+diogoesnog@users.noreply.github.com>
2020-12-09 00:06:10 +0000

    Mais Updates

--- Frontend/src/views/Login.vue
+++ Frontend/src/views/Login.vue
@@ -10,16 +10,12 @@
     <div v-if="loginForm" style="padding-left:30px; padding-right:30px">
       <LoginForm/>
       <br/>
-<<<<<<< Updated upstream
-      <span @click="loginForm = !loginForm"><q-btn color="secondary" icon="how_to_reg" :label="$t('loginPage.noAccount')"></q-btn></span>
-=======
       <p>
         {{$t('loginPage.noAccount')}}
         <span @click="loginForm = !loginForm">
           <b>{{$t('loginPage.createAccount')}}</b>
         </span>
       </p>
->>>>>>> Stashed changes
     </div>
 
     <div v-else>
@@ -28,19 +24,8 @@
       <!-- <q-btn push color="primary" style="width: 100%" @click="loginForm = !loginForm">
         {{ $t('commonButtons.cancel')}}
       </q-btn> -->
-<<<<<<< Updated upstream
-        <div class="row" style="margin-top:10px">
-          <div class="col-6">
-            <p class="text-right">{{$t('registerPage.alreadyHasAccount')}}</p>
-          </div>
-          <div class="col-6">
-            <span style="margin-left:10px"  @click="loginForm = !loginForm"> <b>{{$t('registerPage.login')}}</b></span>
-          </div>
-        </div>
-=======
       <p class="text-center" style="margin-top:35px">{{$t('registerPage.alreadyHasAccount')}}</p>
       <span class="absolute-center" style="margin-top:95%" @click="loginForm = !loginForm"> <b>{{$t('registerPage.login')}}</b></span>
->>>>>>> Stashed changes
     </div>
   </div>
 </template>
@@ -73,9 +58,6 @@
 
 <style scoped>
 
-<<<<<<< Updated upstream
-
-=======
   .centerDiv {
     width: 75%;
     position: absolute;
@@ -85,6 +67,5 @@
     transform: translate(-50%, -50%);
     text-align: center;
   }
->>>>>>> Stashed changes
 
 </style>
