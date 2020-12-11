aa197b9e620693ca0e01369f70e6692d5536f287
André Tiago <andre.tiago.at@gmail.com>
2020-12-11 17:00:07 +0000

    check if store is open + save date in UTC

--- Backend/Booking/app/routes/api/booking.js
+++ Backend/Booking/app/routes/api/booking.js
@@ -8,31 +8,37 @@ app.get('/', (req, res) => {
         .then(data => {
             res.status(200).jsonp(data);
         }).catch(err => {
-            res.status(500).jsonp(err);
-        });
+        res.status(500).jsonp(err);
+    });
 });
 
 app.post('/', async (req, res) => {
     const booking = {
-        bookingDate: new Date(Date.now()),
-        serviceDate: new Date(req.body.serviceDate),
+        bookingDate: new Date(Date.now()).toISOString(),
+        serviceDate: new Date(req.body.serviceDate).toISOString(),
         userId: req.body.userId,
         storeId: req.body.storeId
     };
 
-    if (new Date(Date.now()) > booking.serviceDate) {
+    if (new Date(Date.now()) > new Date(booking.serviceDate)) {
         res.status(400).jsonp({msg: "Invalid Date"});
     }
     else if (await Booking.dateExists(booking.serviceDate, booking.storeId)) {
         res.status(400).jsonp({msg: "There is already a service scheduled for that time"});
     }
     else {
-        Booking.createBooking(booking)
-            .then(data => {
-                res.status(200).jsonp(data);
-            }).catch(err => {
-            res.status(500).jsonp(err);
-        });
+        serviceOpen = isOpen(new Date(booking.serviceDate), req.body.schedule);
+
+        if (serviceOpen) {
+            Booking.createBooking(booking)
+                .then(data => {
+                    res.status(200).jsonp(data);
+                }).catch(err => {
+                res.status(500).jsonp(err);
+            });
+        } else {
+            res.status(500).jsonp({msg: "The service is closed"});
+        }
     }
 });
 
@@ -46,23 +52,49 @@ app.delete('/:id', (req, res) => {
 
 app.put('/', async (req, res) => {
     const id = req.body.id;
-    const bookingDate = new Date(Date.now());
-    const serviceDate = new Date(req.body.serviceDate);
+    const bookingDate = new Date(Date.now()).toISOString();
+    const serviceDate = new Date(req.body.serviceDate).toISOString();
     const storeId = (await Booking.getStoreFromID(id)).storeId;
 
-    if (new Date(Date.now()) > serviceDate) {
+    if (new Date(Date.now()) > new Date(serviceDate)) {
         res.status(400).jsonp({msg: "Invalid Date"});
     }
     else if (await Booking.dateExists(serviceDate, storeId)) {
         res.status(400).jsonp({msg: "There is already a service scheduled for that time"});
     }
     else {
-        Booking.reschedule(id, bookingDate, serviceDate)
-            .then(data => {
-                res.status(200).jsonp(data);
-            })
-            .catch(err => res.status(400).jsonp(err));
+        serviceOpen = isOpen(new Date(serviceDate), req.body.schedule);
+
+        if (serviceOpen) {
+            Booking.reschedule(id, bookingDate, serviceDate)
+                .then(data => {
+                    res.status(200).jsonp(data);
+                })
+                .catch(err => res.status(400).jsonp(err));
+        } else {
+            res.status(500).jsonp({msg: "The service is closed"});
+        }
     }
 });
+
+function isOpen (serviceDate, schedule) {
+    const weekService = serviceDate.toLocaleTimeString('en-us', {weekday: 'long'}).split(' ')[0];
+
+    for (let item of schedule) {
+        if (item.day === weekService) {
+            const closingHour = item.closingHour.split(':');
+            const openingHour = item.openingHour.split(':');
+            const serviceHour = [serviceDate.getUTCHours(), serviceDate.getUTCMinutes()];
+
+            if (serviceHour[0] > openingHour[0] && serviceHour[0] < closingHour[0])
+                return true;
+            else if (serviceHour[0] == openingHour[0] && serviceHour[1] > openingHour[1])
+                return true;
+            else if (serviceHour[0] == closingHour[0] && serviceHour[1] < closingHour[1])
+                return true;
+        }
+    }
+    return false;
+}
 
 module.exports = app;
