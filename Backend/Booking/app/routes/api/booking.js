0c4080a3784afc8892dc6b2c07579d3976481732
André Tiago <andre.tiago.at@gmail.com>
2020-12-13 15:08:31 +0000

    Auth Middleware

--- Backend/Booking/app/routes/api/booking.js
+++ Backend/Booking/app/routes/api/booking.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express.Router();
 const Booking = require('../../controllers/booking');
+const checkAuth = require('../../middlewares/checkAuth');
 
 /**
  * Get list of reservations
