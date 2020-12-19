fdff6f7330a394bd5a515aad92be5d1e6652c14b
miguelsolans <dev.miguelsolans@gmail.com>
2020-12-19 17:12:21 +0000

    + Added Catalog and Reviews Endpoints

--- Backend/Gateway/app/services/Stores/catalog.js
+++ Backend/Gateway/app/services/Stores/catalog.js
@@ -1,6 +1,30 @@
 const Request = require('../../utils/Request');
 
 
-module.exports.getAll = () => {
-    // let request = new Request(`${process.env.}`);
+module.exports.getByStore = (store) => {
+    let request = new Request(`${process.env.STORE_SERVICE_ENDPOINT}/catalogs/store/${store}`);
+
+    request.isJson();
+
+    return request.get();
+
 }
+
+module.exports.addCatalog = (store, body) => {
+    let request = new Request(`${process.env.STORE_SERVICE_ENDPOINT}/catalogs/${store}`);
+
+    request.isJson();
+
+    return request.post(body);
+
+}
+
+// TODO: Too confusing, ask for further explanations
+module.exports.deleteCatalog = (store) => {
+    let request = new Request(`${process.env.STORE_SERVICE_ENDPOINT}/catalogs/store/${store}`);
+
+    request.isJson();
+
+    return request.delete();
+}
+
