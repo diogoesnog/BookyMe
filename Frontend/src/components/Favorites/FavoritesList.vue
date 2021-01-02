596c1ac7ba406665f0d5236a3f34746cb019ed04
minesvieira <56404783+minesvieira@users.noreply.github.com>
2021-01-02 16:50:36 +0000

    update

--- Frontend/src/components/Favorites/FavoritesList.vue
+++ Frontend/src/components/Favorites/FavoritesList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="q-pa-md" style="padding:35px">
       <q-img
-      src="https://cdn.quasar.dev/img/parallax2.jpg"
+      :src="getImage()"
       spinner-color="white"
       style="height: 170px; max-width: 300px"
       img-class="my-custom-image"
@@ -31,6 +31,17 @@ export default {
       favorites: Array
     }
   },
+
+  methods: {
+    getImage() {
+      let numberPhotos = this.photos.length;
+      if(numberPhotos > 0) {
+        let i = Math.floor(Math.random() * (numberPhotos + 1));
+        return `http://localhost:5100${this.photos[0].url}`
+      }
+      return "";
+    }
+  }
 }
 </script>
 
