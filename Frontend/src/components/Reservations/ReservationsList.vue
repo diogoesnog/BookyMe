cdcb09b5943807766fe4577f481ac37685c8c5ec
minesvieira <56404783+minesvieira@users.noreply.github.com>
2021-01-07 00:44:43 +0000

    estrutura construida reservas

--- Frontend/src/components/Reservations/ReservationsList.vue
+++ Frontend/src/components/Reservations/ReservationsList.vue
@@ -1,10 +1,10 @@
 <template>
   <div style="padding-top: 25px; margin: 15px;">
     <div class="oval">
-      <p> Felgueiras </p>
+      <p style="font-weight: 300; font-size: 18px; line-height:50%;" class="text-white"> Felgueiras </p>
     </div>
     <br>
-    <div class="information">
+    <div class="information shadow">
       <div class="row">
         <div class="col-4">
           <q-img
@@ -16,6 +16,7 @@
           <p style="font-weight: 670; font-size: 22px; line-height:0%;"> Ginásio Go Fitness </p>
           <p style="font-weight: 300; font-size: 18px; line-height:50%;"> Sessão CrossFit </p>
           <p style="font-weight: 670; font-size: 22px; line-height:0%; margin-top:50px"> 13 Nov - 17:00 </p>
+          <q-icon roudned class="fas fa-ellipsis-v" style="font-size: 1.5em; margin-left:180px" />
         </div>
       </div>
     </div>
@@ -47,6 +48,12 @@ export default {
   width: 330px;
   background-color:white;
   margin-left: 5px;
+  border-radius: 28px;
+}
+
+.shadow {
+  box-shadow: 0 0px 15px rgba(0, 0, 0, 0.1);
+  border-radius: 28px;
   border-radius: 28px;
 }
 </style>
