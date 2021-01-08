3ad39ca9d3497c13841fd23fa34f37dc1383783d
Sarah250 <Sarah250@github.com>
2021-01-08 17:12:12 +0000

    Card component created
    Minor changes in Store.vue

---
+++ backoffice/src/components/Dashboard/Cards.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="3">
+        <v-card
+            class="pa-2"
+            color="#F44336"
+            elevation="12"
+            tile
+        >
+          <v-card-title
+          >
+            <span class="title">Reservas 1</span>
+          </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="3">
+        <v-card
+            class="pa-2"
+            color="#2196F3"
+            elevation="3"
+            tile
+        >
+          <v-card-title>
+            <span class="title">Reservas 2</span>
+          </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="3">
+        <v-card
+            class="pa-2"
+            color="#4CAF50"
+            elevation="3"
+            tile
+        >
+          <v-card-title>
+            <span class="title">Reservas 3</span>
+          </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="3">
+        <v-card
+            class="pa-2"
+            color="#FFEB3B"
+            elevation="3"
+            tile
+        >
+          <v-card-title>
+            <span class="title">Reservas 4</span>
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cards"
+
+}
+
+</script>
+
--- backoffice/src/views/private/subpages/Store.vue
+++ backoffice/src/views/private/subpages/Store.vue
@@ -2,67 +2,20 @@
   <div>
       <h1 class ="storeName .text-lg-h6">{{this.store.name}}</h1>
     <v-container grid-list-lg>
-      <v-row>
-        <v-col cols="3">
-          <v-card
-              class="pa-2"
-              color="#F44336"
-              elevation="12"
-              tile
-          >
-            <v-card-title
-            >
-              <span class="title">Reservas 1</span>
-            </v-card-title>
-          </v-card>
-        </v-col>
-        <v-col cols="3">
-          <v-card
-              class="pa-2"
-              color="#2196F3"
-              elevation="3"
-              tile
-          >
-            <v-card-title>
-              <span class="title">Reservas 2</span>
-            </v-card-title>
-          </v-card>
-        </v-col>
-      <v-col cols="3">
-        <v-card
-            class="pa-2"
-            color="#4CAF50"
-            elevation="3"
-            tile
-        >
-          <v-card-title>
-            <span class="title">Reservas 3</span>
-          </v-card-title>
-        </v-card>
-      </v-col>
-      <v-col cols="3">
-        <v-card
-            class="pa-2"
-            color="#FFEB3B"
-            elevation="3"
-            tile
-        >
-        <v-card-title>
-          <span class="title">Reservas 4</span>
-        </v-card-title>
-        </v-card>
-      </v-col>
-      </v-row>
+      <Cards>
+      </Cards>
     </v-container>
   </div>
 </template>
 
 <script>
 import Service from "../../../service/user.service"
+import Cards from "../../../components/Dashboard/Cards"
 
 export default {
   name: "StoreDash",
   components:  {
+    Cards
   },
   data() {
     return {
