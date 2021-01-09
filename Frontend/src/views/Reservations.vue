d9acd669c7c3485e476dd4a0d3dcf1441fb168cf
Diogo Nogueira <34174814+diogoesnog@users.noreply.github.com>
2021-01-09 22:19:09 +0000

    Começar Design Reservas

--- Frontend/src/views/Reservations.vue
+++ Frontend/src/views/Reservations.vue
@@ -1,27 +1,25 @@
 <template>
-  <div>
-    <br>
-    <div style="color: #434343">
-      <div class="row">
-        <div class="col-6">
-          <q-btn padding="6px 6px" class="gradientOne shadow" round icon="fas fa-angle-left"/>
-        </div>
+  <div style="color: #434343;" class="centerDiv">
+    <div class="row" style="margin: 20px; padding-top:10px;">
+      <div class="col-10" style="">
+        <p>
+          <span style="font-weight: 670; font-size: 45px;">
+            {{$t('bookingsPage.title')}}
+          </span>
+          <br/>
+          <span style="font-weight: 300; font-size: 20px;">
+            {{$t('bookingsPage.description')}}
+          </span>
+        </p>
       </div>
-      <div class="row">
-        <div class="col">
-          <p style="font-weight: 670; font-size: 45px; margin-top:10px; margin-bottom:0px; padding:15px">{{$t('bookingsPage.title')}}</p>
-        </div>
-        <div class="col" style="margin-right:5px">
-          <q-btn
-            round
-            class="filterAvatar"
-            size="md" 
-            icon="fas fa-filter"
-            style="margin-left:110px; margin-top:30px"
-          />
-        </div>
+      <div class="col-2" style="padding-left: 15px; display: flex; align-items: center; justify-content: center;">
+        <q-btn
+          round
+          class="filterAvatar"
+          size="md" 
+          icon="fas fa-filter"
+        />
       </div>
-      <h6 style="padding:15px; margin-top:0px; margin-bottom:0px">{{$t('bookingsPage.description')}}</h6>
     </div>
       <ReservationsList/>
     <div>
@@ -86,10 +84,18 @@ export default {
 
 <style scoped>
 
+  .filterAvatar {
+    font-size: 1em!important;
+    color: white;
+    background-color: #434343;
+  }
+  
+  .centerDiv {
+    padding: 15px;
+    margin-top: 10px;
+  }
+
   .gradientOne {
-    margin-left: 10px;
-    margin-top: 0px;
-    position: relative;
     background: linear-gradient(#e9695c, #e03459);
     color: white;
   }
