d95b3247a9b8624553901a1c2252529b9f9ec950
marianallc <mariana.lino.costa@sapo.pt>
2021-01-15 00:34:11 +0000

    Design botões da página do perfil pronto
    Co-Authored-By: bvc97 <brunavieiracruz@gmail.com>

--- Frontend/src/components/Profile/ProfileData.vue
+++ Frontend/src/components/Profile/ProfileData.vue
@@ -40,66 +40,86 @@
         </p>
       </div>
       <!-- Primeira Box - Dados Pessoais -->
-      <div class="roundedDiv shadow" style="padding:12px; display: grid; border-radius: 30px; font-weight: 280; font-size: 13px; text-align: left; ">
-         <!-- Botões -->
-        <div style="text-align: right; padding-left: 245px; position: absolute;">
-          <q-btn flat size="md" class="" @click="bottomEdit" style="text-transform: capitalize; font-size: 15px; font-weight: 350;" v-show= "!esconde"   >
-            {{ $t('profilePage.editData') }}
-          </q-btn>
-        </div>
-        <div>
-           <q-btn @click="bottomSave" label="Save" color="primary" v-show= "esconde" />
-        </div>
-        <div >
-          <q-btn @click="bottomCancel" label="Cancel" color="primary" v-show= "esconde" />
-        </div>
-      
-        <!-- Nome -->
-        <div style="margin-left: 10px; margin-top: 15px;">
-          <q-form  class="q-gutter-md">
-              <q-input borderless v-model="profile.name"  style="height: 30px;" type="text" :disable ="!editavel">
-                <template v-slot:prepend>
-                    <q-icon name="fas fa-user" color="grey-5" style="font-size: 20px; font-weight:350"/>
-                </template>
-              </q-input>
-            </q-form>
-        </div>
-        <!-- Nome de Utilizador -->
-        <div style="margin-left: 10px;">
-          <q-form  class="q-gutter-md">
-          <q-input borderless v-model="profile.username" style="height: 30px;" :disable ="!editavel">
-            <template v-slot:prepend>
-              <span style="font-size: 28px; font-weight:350">#</span>
-            </template>
-          </q-input>
-          </q-form>
-        </div>
-        <!-- Email -->
-        <div style="margin-left: 10px;">
-          <q-input borderless v-model="profile.email" style="height: 30px;" :disable ="!editavel" >
-            <template v-slot:prepend>
-              <span style="font-size: 20px; font-weight:350">@</span>
-            </template>
-          </q-input>
-        </div>
-        <!-- Morada -->
-        <div class="row">
-          <div class="col-1" style="display: flex; justify-content: left; align-items: center; padding: 10px;">
-            <q-icon name="fas fa-home" color="grey-5" style="font-size: 20px; "/>
-          </div>
-          <div class="col-11" style="padding-left: 15px">
-            <q-input borderless v-model="profile.address" style="height: 30px;" :disable ="!editavel"/>
-            <div class = "row" style="margin-top: -10px;">
-              <div class= "col-3">
-                <q-input borderless v-model="profile.zipCode" style="height: 30px;" :disable ="!editavel"/> 
+      <div class="roundedDiv shadow" style="display: grid; border-radius: 30px; font-weight: 280; font-size: 13px; text-align: left; ">
+
+        <div class="row" style="padding: 5px">
+                
+                <div class="col-10" style="padding: 10px">
+                  <!-- Nome -->
+                  <div style="margin-left: 10px; margin-top: 15px;">
+                    <q-form  class="q-gutter-md">
+                        <q-input borderless v-model="profile.name"  style="height: 30px;" type="text" :disable ="!editavel">
+                          <template v-slot:prepend>
+                              <q-icon name="fas fa-user" color="grey-5" style="font-size: 20px; font-weight:350"/>
+                          </template>
+                        </q-input>
+                      </q-form>
+                  </div>
+                  <!-- Nome de Utilizador -->
+                  <div style="margin-left: 10px;">
+                    <q-form  class="q-gutter-md">
+                    <q-input borderless v-model="profile.username" style="height: 30px;" :disable ="!editavel">
+                      <template v-slot:prepend>
+                        <span style="font-size: 28px; font-weight:350">#</span>
+                      </template>
+                    </q-input>
+                    </q-form>
+                  </div>
+                  <!-- Email -->
+                  <div style="margin-left: 10px;">
+                    <q-input borderless v-model="profile.email" style="height: 30px;" :disable ="!editavel" >
+                      <template v-slot:prepend>
+                        <span style="font-size: 20px; font-weight:350">@</span>
+                      </template>
+                    </q-input>
+                  </div>
+                  <!-- Morada -->
+                  <div class="row">
+                    <div class="col-1" style="display: flex; justify-content: left; align-items: center; padding: 10px;">
+                      <q-icon name="fas fa-home" color="grey-5" style="font-size: 20px; "/>
+                    </div>
+                    <div class="col-11" style="padding-left: 15px">
+                      <q-input borderless v-model="profile.address" style="height: 30px;" :disable ="!editavel"/>
+                      <div class = "row" style="margin-top: -10px;">
+                        <div class= "col-3">
+                          <q-input borderless v-model="profile.zipCode" style="height: 30px;" :disable ="!editavel"/> 
+                        </div>
+                        <div class= "col-9">
+                          <q-input borderless v-model="profile.city" style="height: 30px;" :disable ="!editavel"/>
+                        </div>  
+                      </div>
+                    </div>
+                  </div>
               </div>
-              <div class= "col-9">
-                <q-input borderless v-model="profile.city" style="height: 30px;" :disable ="!editavel"/>
-              </div>  
-            </div>
-          </div>
+
+              <div class="col-2" style="padding: 10px">
+                  <!-- Botões -->
+                  <div style="text-align: right; position: absolute;" >
+                    <q-btn  size="sm" class="gradientBlue" round @click="bottomEdit" v-show= "!esconde"   >
+                      <q-icon name="fas fa-pen" color="white" />
+                    </q-btn>
+                  </div> 
+                  <div>
+                    <q-btn  size="sm" class="gradientBlue" round @click="bottomSave" v-show= "esconde" >
+                        <q-icon name="fas fa-save" color="white" />
+                    </q-btn>
+                  </div>
+                  <div >
+                    <q-btn   size="sm" class="gradientBlue" round @click="bottomCancel" v-show= "esconde" style = "margin-top : 5px">
+                        <q-icon name="fas fa-times" color="white" />
+                    </q-btn>
+                  </div>
+              </div>
         </div>
-      </div>
+    </div>
+      
+      
+      
+      
+      
+      
+      
+      
       <!-- PARTE DA SEGURANÇA -->
       
       <!-- Box Vermelha - Segurança -->
@@ -109,54 +129,65 @@
           </p>
       </div>
       <!-- Segunda Box - password -->
-      <div class="roundedDiv shadow" style="padding:30px; display: grid; border-radius: 30px; font-weight: 280; font-size: 13px; text-align: left; margin-top:50px;">
-        
-        <!-- BOTÃO CHANGE PASSWORD -->
-        <div style="text-align: right; padding-left: 175px; position: absolute;" v-show= "!esconde2" >
-          <q-btn flat size="md" class="" @click="bottomEdit2" style="text-transform: capitalize; font-size: 15px; font-weight: 350;"   >
-            {{ $t('profilePage.changePassword') }}
-          </q-btn>
-        </div>
-        <div>
-           <q-btn @click="bottomSavePassword" label="Save" color="primary" v-show= "esconde2" />
-        </div>
-        <div >
-          <q-btn @click="bottomCancel2" label="Cancel" color="primary" v-show= "esconde2" />
-        </div>
-        
-        
-        <div style="margin-left: 10px; margin-top:7px">
-         
-         
-         <q-form  class="q-gutter-md">
-           <div class="row" style="padding: 5px">
-                <div class="col-6" style="padding: 10px">
-                  <q-input borderless v-model="newPassword" :label= "$t('profilePage.newPassword')" style="height: 30px;" :type="isPwd ? 'password' : 'text'" :disable ="!editavel2">
-                    
-                   <!--  <template v-slot:prepend>
-                        <q-icon name="fas fa-lock" color="grey-5" />
-                    </template> -->
-                    <template v-slot:append>
-                          <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd"/>
-                    </template>
+      <div class="roundedDiv shadow" style=" display: grid; border-radius: 30px; font-weight: 200; text-align: left; margin-top:50px;">
+        <div class="row" style="padding: 5px">
+            
+            
+          <div class="col-10" >  
+            <div style=" margin-top:7px">
+              <q-form  class="q-gutter-md">
+                <div class="row" style="padding: 5px">
+                      
+                      <div class="col-6" style="padding: 10px">
+                            <q-input borderless v-model="oldPassword" :label= "$t('profilePage.oldPassword')" style="height: 30px;" :type="isPwd ? 'password' : 'text'" :disable ="!editavel2">
+                              
+                              
+                              <template v-slot:append>
+                                    <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd"/>
+                              </template>
 
-                  </q-input>
+                            </q-input>
+                      </div>
+                      <div class="col-6" style="padding: 10px">
+                        <q-input borderless v-model="newPassword" :label= "$t('profilePage.newPassword')" style="height: 30px;" :type="isPwd ? 'password' : 'text'" :disable ="!editavel2">
+                          
+                        <!--  <template v-slot:prepend>
+                              <q-icon name="fas fa-lock" color="grey-5" />
+                          </template> -->
+                          <template v-slot:append>
+                                <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd"/>
+                          </template>
 
+                        </q-input>
+
+                      </div>
                 </div>
-                <div class="col-6" style="padding: 10px">
-                      <q-input borderless v-model="oldPassword" :label= "$t('profilePage.oldPassword')" style="height: 30px;" :type="isPwd ? 'password' : 'text'" :disable ="!editavel2">
-                        
-                        
-                        <template v-slot:append>
-                              <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd"/>
-                        </template>
-
-                      </q-input>
-                  </div>
+              </q-form>
             </div>
-          </q-form>
+          </div>
+            <!-- BOTÃO CHANGE PASSWORD -->
+          <div class="col-2" style="padding: 10px">
+            <div style="text-align: right;  position: absolute;"  >
+              <q-btn flat size="sm" class="gradientBlue" round @click="bottomEdit2" v-show= "!esconde2"  >
+                <q-icon name="fas fa-pen" color="white" />
+              </q-btn>
+            </div>
+            <div>
+              <q-btn flat size="sm" class="gradientBlue" @click="bottomSavePassword" round v-show= "esconde2" >
+                  <q-icon name="fas fa-save" color="white" />
+              </q-btn>
+            </div>
+            <div >
+              <q-btn flat size="sm" class="gradientBlue" @click="bottomCancel2" round v-show= "esconde2" style = "margin-top : 5px">
+                <q-icon name="fas fa-times" color="white" />
+              </q-btn>
+            </div>
+          </div>
         </div>
       </div>
+
+
+
       <!-- Botão Logout -->
       <q-btn class="gradientBlue" rounded @click="handleLogout" style="width: 200px; height: 35px; margin: 40px; bottom:10px;">
         <p style="font-size:130%; text-transform: capitalize;" class="font-weight-bold button">
