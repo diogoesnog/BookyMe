5dbf23b7a3e4b38b679bfa015546f8aa23c15186
dichico <36507885+dichico@users.noreply.github.com>
2021-01-16 19:42:47 +0000

    Esqueleto da página e script do Reservation.vue

---
+++ Frontend/src/views/Reservation.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <StoreBanner v-bind="storeData"/>
+    <div class="divBottom">
+<!--      TODO: Colocar aqui o componente da parte da reserva. -->
+      <StoreMap v-bind="storeData"/>
+      <StoreCatalog v-bind="storeData"/>
+      <StoreRatings v-bind="storeData"/>
+      <StoreBooking v-bind="storeData"/>
+    </div>
+    <Toolbar/>
+  </div>
+</template>
+
+<script>
+
+import Toolbar from 'components/Root/Toolbar';
+import Service from '../services/user.service'
+import StoreBanner from 'components/Store/StoreBanner';
+import StoreRatings from "components/Store/StoreRatings";
+import StoreMap from "components/Store/StoreMap";
+import StoreCatalog from "components/Store/StoreCatalog";
+import StoreBooking from "components/Store/StoreBooking";
+
+export default {
+
+  name: "Reservation",
+  components: {
+    StoreBanner,
+    StoreMap,
+    StoreCatalog,
+    StoreRatings,
+    StoreBooking,
+    Toolbar
+  },
+
+  data() {
+    return {
+      storeID: this.$route.params.id,
+      storeData: Object,
+      lang: this.$i18n.locale,
+      langOptions: [
+        { value: 'en-us', label: this.$t('languages.english')},
+        { value: 'pt', label: this.$t('languages.portuguese')},
+        { value: 'fr', label: this.$t('languages.french')}
+      ]
+    }
+  },
+
+  watch: {
+    lang(lang) {
+      console.log("Language Change", lang);
+      this.$i18n.locale = lang.value;
+    }
+
+  },
+
+  mounted() {
+    console.log("Mounted: View has been rendered");
+    this.fetchStoreData();
+  },
+
+  methods: {
+    fetchStoreData() {
+      this.$q.loading.show({ delay: 400});
+      Service.getStoreData(this.storeID)
+        .then(response => {
+          let data = response.data["data"];
+          this.storeData = data[0];
+          console.log(this.storeData);
+        }).catch(err => console.log(err)
+      ).finally(() => {
+        this.$q.loading.hide();
+      })
+    }
+  }
+
+}
+
+</script>
+
+<style scoped>
+
+  .divBottom {
+    position: relative;
+    padding-top: 80px;
+    padding-left: 0;
+  }
+
+</style>
