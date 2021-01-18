a2ab8ac91cd6fea7234b8ecbd4ce0ef4bfdbd54a
dichico <36507885+dichico@users.noreply.github.com>
2021-01-18 23:40:58 +0000

    Primeira tentativa de seleção de serviços

--- Frontend/src/components/Store/StoreBooking.vue
+++ Frontend/src/components/Store/StoreBooking.vue
@@ -1,56 +1,100 @@
 <template>
   <div>
-    <q-btn class="botao" rounded label="Faça a sua marcação!" @click="card = true" color="vermelho" icon="event_available"/>
+    <q-btn class="botao" rounded label="Faça a sua marcação!" @click="cardBook = true" color="vermelho" icon="event_available"/>
 
-    <q-dialog v-model="card">
-<!--      TODO: Fazer a parte de seleção de serviço -->
-      <q-card class="my-card">
-        <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+    <q-dialog v-model="cardBook">
+      <q-carousel
+        transition-prev="slide-right"
+        transition-next="slide-left"
+        animated
+        v-model="slide"
+        control-color="primary"
+        navigation-icon="radio_button_unchecked"
+        navigation-active-icon="radio_button_checked"
+        navigation
+        arrows
+        padding
+        height="200px"
+        class="bg-white shadow-1 rounded-borders"
+      >
+        <q-carousel-slide :name="1" class="column no-wrap flex-center">
+          <div class="q-mt-md text-center">
+            <p style="color: #434343; font-weight: 700; font-size: 15px;">{{name}}</p>
+            <q-select
+              label="Selecione o(s) seu(s) serviço(s)"
+              style="width: 200px"
+              outlined
+              transition-show="scale"
+              transition-hide="scale"
+              multiple
+              counter
+              hint="Serviços marcados"
+              v-model="services"
+              :options="options"
 
-        <q-card-section>
-          <q-btn
-            fab
-            color="vermelho"
-            icon="event_available"
-            class="absolute"
-            style="top: 0; right: 12px; transform: translateY(-50%);"
-          />
-
-          <div class="row no-wrap items-center">
-            <div class="col text-h6 ellipsis" style="margin-top: 10px; color: #434343; font-weight: 700; font-size: 20px;">
-              {{ this.name }}
-            </div>
+            />
           </div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <q-date
-              :title="$t('storePage.date')"
-              today-btn
-              mask="YYYY-MM-DDTHH:mm"
-              :subtitle="$t('storePage.dateSub')"
-              color="red"
-              v-model="date" />
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <q-time
-            v-model="date"
-            format24h
-            mask="YYYY-MM-DDTHH:mm"
-            color="red" />
-        </q-card-section>
-
-        <q-separator/>
-
-        <q-card-actions>
-            <q-btn icon="event" @click="makeBooking" style="margin-left: 100px" rounded color="vermelho">
-              {{$t('storePage.book')}}
-            </q-btn>
-        </q-card-actions>
-
-      </q-card>
+        </q-carousel-slide>
+        <q-carousel-slide :name="2" class="column no-wrap flex-center">
+          <div class="q-mt-md text-center">
+            Selecionar o seu slot de marcação
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide :name="3" class="column no-wrap flex-center">
+          <q-btn>Marque já</q-btn>
+        </q-carousel-slide>
+      </q-carousel>
     </q-dialog>
+
+    <!--    <q-dialog v-model="card">
+    &lt;!&ndash;      TODO: Fazer a parte de seleção de serviço &ndash;&gt;
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
+            <q-card-section>
+              <q-btn
+                fab
+                color="vermelho"
+                icon="event_available"
+                class="absolute"
+                style="top: 0; right: 12px; transform: translateY(-50%);"
+              />
+
+              <div class="row no-wrap items-center">
+                <div class="col text-h6 ellipsis" style="margin-top: 10px; color: #434343; font-weight: 700; font-size: 20px;">
+                  {{ this.name }}
+                </div>
+              </div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+              <q-date
+                  :title="$t('storePage.date')"
+                  today-btn
+                  mask="YYYY-MM-DDTHH:mm"
+                  :subtitle="$t('storePage.dateSub')"
+                  color="red"
+                  v-model="date" />
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+              <q-time
+                v-model="date"
+                format24h
+                mask="YYYY-MM-DDTHH:mm"
+                color="red" />
+            </q-card-section>
+
+            <q-separator/>
+
+            <q-card-actions>
+                <q-btn icon="event" @click="makeBooking" style="margin-left: 100px" rounded color="vermelho">
+                  {{$t('storePage.book')}}
+                </q-btn>
+            </q-card-actions>
+
+          </q-card>
+        </q-dialog>-->
   </div>
 </template>
 
@@ -67,14 +111,20 @@ name: "StoreBooking",
   data() {
     return {
       storeID: this.$route.params.id,
-      card: Boolean,
+      cardBook: false,
+      slide: 1,
+      lorem: "Teste",
       date: String,
-      booking: new Booking()
+
+      options: [
+        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+      ],
+      booking: new Booking(),
+      services: null
     }
   },
   beforeMount() {
-    this.date = new Date("2021/01/15").toLocaleString();
-    this.card = false;
+
   },
   methods: {
     makeBooking() {
@@ -122,7 +172,5 @@ name: "StoreBooking",
 .bg-vermelho {
   background: linear-gradient(#e9695c, #e03459);
 }
-
-
 
 </style>
